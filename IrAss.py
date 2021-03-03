8125a96418bd6d33dce8ed6c19ca13051a741084
Aditya <adijr9487@gmail.com>
2021-03-03 11:10:37 +0530

    2nd

--- IrAss.py
+++ IrAss.py
@@ -6,86 +6,73 @@ def get_input():
 
     print('Program to transform frame A to frame B, then input position of a point in a frame and output position of the same point in another frame')
     print('\nFollowing transformations can be performed:')
-    print('1. Rotation about x-axis')
-    print('2. Rotation about y-axis')
-    print('3. Rotation about z-axis')
-    print('4. Translation about x-axis')
-    print('5. Translation about y-axis')
-    print('6. Translation about z-axis')
+    print('1. Enter rotation about x-axis')
+    print('2. Enter rotation about y-axis')
+    print('3. Enter rotation about z-axis')
+    print('4. enter translation about x-axis')
+    print('5. enter translation about y-axis')
+    print('6. enter translation about z-axis')
 
-    # Angle in degree
-    rotation_x = int(input('\nEnter angle to rotate x-axis (in degrees): '))
-    rotation_x = math.pi/180*rotation_x
-    rotation_y = int(input('Enter angle to rotate y-axis (in degrees): '))
-    rotation_y = math.pi/180*rotation_y
-    rotation_z = int(input('Enter angle to rotate z-axis (in degrees): '))
-    rotation_z = math.pi/180*rotation_z
+    r_x = math.pi/180*int(input('\nangle to rotate x-axis: '))
+    r_y =  math.pi/180*int(input('angle to rotate y-axis: '))
+    r_z =  math.pi/180*int(input('angle to rotate z-axis: '))
 
-    translation_x = int(input('Enter translation from x-axis: '))
-    translation_y = int(input('Enter translation from y-axis: '))
-    translation_z = int(input('Enter translation from z-axis: '))
+    t_x = int(input('translation x-axis: '))
+    t_y = int(input('translation y-axis: '))
+    t_z = int(input('translation z-axis: '))
 
-    frame_known = ''
+    frame_known = "" ;
     while frame_known != 'a' and frame_known != 'b':
         frame_known = input('Point is known in frame? (A/B): ').lower()
         if frame_known != 'a' and frame_known != 'b':
-            print('Two frames are \'A\' or \'B\'')
+            print('Enter either A or B')
 
-    initial_coordinates = np.array(list(
-        map(int, input('\nEnter position of the point in the frame: ').split())))
+    init_coord = np.array(list(
+        map(int, input('\nEnter the coordianate of the point of the frame: ').split())))
 
-    # returns: coordinates given by user and transformation matrix for given transformations
-    return initial_coordinates, calculate_transformation_matrix(rotation_x, rotation_y, rotation_z, translation_x, translation_y, translation_z, frame_known)
+    return init_coord, transf_matrix(r_x, r_y, r_z, t_x, t_y, t_z, frame_known)
 
 
-def calculate_transformation_matrix(rotation_x, rotation_y, rotation_z, translation_x, translation_y, translation_z, frame_known):
+def transf_matrix(rotation_x, rotation_y, rotation_z, translation_x, translation_y, translation_z, frame_known):
 
-    # rotation matrix for rotation about x-axis
-    rx = np.array([[1, 0, 0],
-                   [0, math.cos(rotation_x), -math.sin(rotation_x)],
-                   [0, math.sin(rotation_x), math.cos(rotation_x)]])
+    Rx = np.array([
+            [1, 0, 0],
+            [0, math.cos(rotation_x), -math.sin(rotation_x)],
+            [0, math.sin(rotation_x), math.cos(rotation_x)]]
+        )
 
-    # rotation matrix for rotation about y-axis
-    ry = np.array([[math.cos(rotation_y), 0, math.sin(rotation_y)],
-                   [0, 1, 0],
-                   [-math.sin(rotation_y), 0, math.cos(rotation_y)]])
+    Ry = np.array([
+            [math.cos(rotation_y), 0, math.sin(rotation_y)],
+            [0, 1, 0],
+            [-math.sin(rotation_y), 0, math.cos(rotation_y)]
+        ]
+        )
 
-    # rotation matrix for rotation about z-axis
-    rz = np.array([[math.cos(rotation_z), -math.sin(rotation_z), 0],
-                   [math.sin(rotation_z), math.cos(rotation_z), 0],
-                   [0, 0, 1]])
+    Rz = np.array([
+            [math.cos(rotation_z), -math.sin(rotation_z), 0],
+            [math.sin(rotation_z), math.cos(rotation_z), 0],
+            [0, 0, 1]
+        ])
 
-    # final rotation matrix is the matrix multiplication of all three rotation matrices
-    r = rz @ ry @ rx
+    R_multiplication = Rz @ Ry @ Rx
 
-    # if point is known is frame 'A' we need inverse transformation matrix
     if frame_known == 'a':
-        # displacement matrix representing displacement about 3 axes
-        d = np.array([[translation_x], [translation_y], [translation_z]])
-        temp = -r.T@d
-        temp = np.vstack((temp, [1]))
-
-        transformation_matrix = np.hstack(
-            (np.vstack((r.T, np.array([0, 0, 0]))), temp))
-
-        return transformation_matrix
+        temp = np.vstack((-R_multiplication.T@np.array([[translation_x], [translation_y], [translation_z]]), [1]))
+        return np.hstack((np.vstack((R_multiplication.T, np.array([0, 0, 0]))), temp))
 
     elif frame_known == 'b':
-        # displacement matrix representing displacement about 3 axes
         d = np.array([[translation_x], [translation_y], [translation_z], [1]])
 
         transformation_matrix = np.hstack(
-            (np.vstack((r, np.array([0, 0, 0]))), d))
+            (np.vstack((R_multiplication, np.array([0, 0, 0]))), d))
 
         return transformation_matrix
 
 
 def transform(initial_coordinates, transformation_matrix):
-    # 1 is appending to the coordinates for matrix multiplication
     initial_coordinates = np.hstack((initial_coordinates, 1))
     final_coordinates = transformation_matrix@initial_coordinates
 
-    # removing '1' in the last dimension
     return final_coordinates[:3]
 
 
@@ -94,7 +81,7 @@ def print_final_answer(final_coordinates):
           final_coordinates[0], final_coordinates[1], final_coordinates[2]))
 
 
-if __name__ == '__main__':
+if _name_ == '_main_':
     initial_coordinates, transformation_matrix = get_input()
     final_coordinates = transform(initial_coordinates, transformation_matrix)
     print_final_answer(final_coordinates)
